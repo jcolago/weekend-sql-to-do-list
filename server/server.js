1c7b3420b3c85afab4ed14ad07a4259bd8b34d1b
jcolago <jcolago2005@gmail.com>
2023-09-15 16:51:39 -0500

    Set up GET and POST routes in server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,38 @@
 //Hooking up express
 const express = require("express");
+const pool = require("./modules/pool");
 const app = express();
 
 app.use(express.static("server/public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 5000;
 
+app.get("/tasks", (req, res) => {
+    console.log('GET request to /tasks');
+    const queryText = `SELECT * FROM "tasks" ORDER BY "id" ASC;`;
+
+    pool
+        .query(queryText)
+        .then((result) => res.send(result.rows))
+        .catch((err) => {
+            console.log('Error in GET reqyest', err);
+            res.sendStatus(500)
+        });
+});
+
+app.post("/tasks", (req, res) => {
+    console.log('POST request sent to /tasks');
+    const task = req.body;
+    console.log('POST request at /tasks with data of', task);
+    res.sendStatus(201);
+});
 
 
 
 
 
 
-
-
-
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log("listening on port", PORT);
 });
