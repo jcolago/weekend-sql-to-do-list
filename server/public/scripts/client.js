665b28e0c259e80b66eb02e5981062b1c7304764
jcolago <jcolago2005@gmail.com>
2023-09-17 14:46:20 -0500

    Added getTasks and appendDom functions to client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,37 @@
 $(() => {
+    console.log('JS and JQ');
     getTasks();
     $('#submitButton').on('click', submitTask);
-})
+});
 
+function getTasks(){
+    $.ajax({
+        method: "GET",
+        url: "/tasks"
+    })
+    .then((response) => appendDom(response))
+    .catch((err) =>{
+        console.log('Error updating list', err)
+    });
+};
+
+function appendDom(response){
+    $("#showTasks").empty();
+    for (const task of response){
+        if (task.completed === true){
+            $("#showTasks").append(`
+            <tr class='completedTask'>
+                <td>${task.task_name}</td>
+                <td><button data-id=${task.id} id="completeButton">Complete</button></td>
+                <td><button data-id=${task.id} id="deletedButton">Delete</button></td>
+            </tr> `);
+        }else (
+            $("#showTasks").append(`
+        <tr>
+            <td>${task.task_name}</td>
+            <td><button data-id=${task.id} id="completeButton">Complete</button></td>
+            <td><button data-id=${task.id} id="deletedButton">Delete</button></td>
+        </tr> `)
+        )
+    };
+};
